2be776c4c42fecc57e5d371456cdf9e85ea57bf7
febriananur <febriananurokhman@gmail.com>
2022-03-02 22:33:46 +0700

    Update tes02022022.ino

--- final/tes02022022/tes02022022.ino
+++ final/tes02022022/tes02022022.ino
@@ -33,6 +33,12 @@ unsigned long lastMsg = 0;
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
+//battery indicator
+int analogInPin  = A0;    // Analog input pin
+int sensorValue; 
+float calibration = 0.36; // Check Battery voltage using multimeter & add/subtract the value
+
+
 void setup_wifi() {
 
   delay(10);
@@ -133,8 +139,7 @@ void reconnect() {
       display.setCursor( 10, 10 );
       display.print("Ready");
       display.display();
-      // Once connected, publish an announcement...
-      client.publish("tes1/sub", "hello world");
+      
       // ... and resubscribe
       client.subscribe("devreminder/node6/pub");
     } else {
@@ -146,6 +151,36 @@ void reconnect() {
     }
   }
 }
+
+void batteryIndicator(){
+  sensorValue = analogRead(analogInPin);
+  voltage = (((sensorValue * 3.3) / 1024) * 2 + calibration); //multiply by two as voltage divider network is 100K & 100K Resistor
+ 
+  bat_percentage = mapfloat(voltage, 2.8, 4.2, 0, 100); //2.8V as Battery Cut off Voltage & 4.2V as Maximum Voltage
+ 
+  if (bat_percentage >= 100)
+  {
+    bat_percentage = 100;
+  }
+  if (bat_percentage <= 0)
+  {
+    bat_percentage = 1;
+  }
+
+  Serial.print("Analog Value = ");
+  Serial.print(sensorValue);
+  Serial.print("\t Output Voltage = ");
+  Serial.print(voltage);
+  Serial.print("\t Battery Percentage = ");
+  Serial.println(bat_percentage);
+  delay(1000);
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 
 void setup() {
   digitalWrite(buzzPin, LOW);
